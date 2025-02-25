f1467463404358a253bdeb655bcaaa3839d39ac0
Chiara Liotta <liotta@astron.nl>
2025-02-25 13:54:38 +0100

    data flow path analysis

--- metric_calculations/CalculationComponentAnalyzer.py
+++ metric_calculations/CalculationComponentAnalyzer.py
@@ -1,4 +1,7 @@
 import csv
+from collections import defaultdict
+import neo4j
+import pandas as pd
 from neo4j import GraphDatabase
 
 class CalculationComponentAnalyzer:
@@ -18,12 +21,13 @@ class CalculationComponentAnalyzer:
         """Queries Neo4j for CalculationComponent fan-in, fan-out, and fan-out set."""
         query = """
         MATCH (cc:CalculationComponent)
-        OPTIONAL MATCH (cc)<-[r_in]-()  // Count all incoming relationships
-        OPTIONAL MATCH (cc)-[r_out]->(target)  // Count all outgoing relationships & get targets
+        OPTIONAL MATCH (cc)<-[r_in]-(target_in)  // Count all incoming relationships
+        OPTIONAL MATCH (cc)-[r_out]->(target_out)  // Count all outgoing relationships & get targets
         RETURN cc.component_id AS component_id, 
                COUNT(DISTINCT r_in) AS fan_in, 
                COUNT(DISTINCT r_out) AS fan_out,
-               COLLECT(DISTINCT target.component_id) AS fan_out_set
+               COLLECT(DISTINCT target_in.component_id) AS fan_in_set,
+               COLLECT(DISTINCT target_out.component_id) AS fan_out_set
         """
         
         with self.driver.session() as session:
@@ -31,8 +35,153 @@ class CalculationComponentAnalyzer:
             return [{"component_id": record["component_id"], 
                      "fan_in": record["fan_in"], 
                      "fan_out": record["fan_out"], 
+                     "fan_in_set": record["fan_in_set"],
                      "fan_out_set": record["fan_out_set"]} 
                     for record in result]
+        
+    def get_outer_workflow_ids(self, component_id):
+        with self.driver.session() as session:
+            query = """
+            MATCH (c:CalculationComponent2 {component_id: $component_id})-[e]-()
+            RETURN collect(distinct e.workflow_id) AS workflows
+            """
+            workflows = set(session.run(query, component_id=component_id).single()["workflows"])
+        return workflows
+    
+    def dfs_path_search(self, component_id_1, component_id_2, workflow_id):
+        lengths = {}
+        bookkeeping = {}
+        with self.driver.session() as session:
+            # Start the recursive DFS from the initial component
+            self.dfs(session, component_id_1, 0, 1, component_id_1, component_id_2, workflow_id, lengths, bookkeeping)
+            return lengths
+
+
+    def dfs(self, session, current_id, path_length, multiplicity, component_id_1, component_id_2, workflow_id, lengths, bookkeeping):
+        """Recursive DFS function."""
+        # Query to find outgoing connections
+        query_1 = """
+        MATCH (c1:CalculationComponent2 {component_id: $current_id})-[r {workflow_id: $workflow_id}]->(c2:CalculationComponent2)
+        RETURN c2.component_id AS next_component_id, elementId(r) AS edge_id
+        """
+
+        sequence = "Output of step (source) used as input for step (target)"
+
+        query_2 = """
+        MATCH (c1:CalculationComponent2 {component_id: $current_id})-[r {workflow_id: $workflow_id, description: $sequence}]->(c2:CalculationComponent2)
+        RETURN c2.component_id AS next_component_id, elementId(r) AS edge_id
+        """
+        if path_length == 0:
+            result = session.run(query_1, current_id=current_id, workflow_id=workflow_id)
+        else:
+            result = session.run(query_2, current_id=current_id, workflow_id=workflow_id, sequence=sequence)
+
+        compressed = {}
+
+        for record in result:
+            next_component_id = record['next_component_id']
+            edge_id = record['edge_id']
+
+            if next_component_id not in compressed:
+                compressed[next_component_id] = set()
+
+            compressed[next_component_id].add(edge_id)
+
+        if current_id in bookkeeping:
+            distances = [path_length + length for length in bookkeeping[current_id]]
+            for distance in distances:
+                if distance not in lengths:
+                    lengths[distance] = 1
+                else:
+                    lengths[distance] += 1
+
+
+        else:
+            bookkeeping[current_id] = list()
+
+            for next_component_id, edge_ids in compressed.items():
+
+                # Check if we reached the target component
+                if next_component_id == component_id_2:
+                    new_length = path_length + 1
+                    bookkeeping[current_id].extend([1] * multiplicity)
+                    if new_length not in lengths:
+                        lengths[new_length] = multiplicity
+                    else:
+                        lengths[new_length] += multiplicity # Store path length
+                # Check for exclusions
+                elif next_component_id != workflow_id \
+                    and workflow_id != component_id_1 and workflow_id != component_id_2:
+                    new_multiplicity = len(edge_ids) * multiplicity
+                    next_bookkeeping = self.dfs(session, next_component_id, path_length + 1, new_multiplicity, component_id_1, component_id_2, workflow_id, lengths, bookkeeping)
+                    bookkeeping[current_id].extend([length + 1 for length in next_bookkeeping])  # Recursive DFS call
+        return bookkeeping[current_id]
+
+
+        
+    def analyze_paths(self, component_id_1, component_id_2, penalty = 1.5):
+        path_lengths = defaultdict(int)
+
+        # 1. Find common workflows
+        c1_workflows =  self.get_outer_workflow_ids(component_id_1)
+        c2_workflows = self.get_outer_workflow_ids(component_id_2)
+        common_workflows = (c1_workflows & c2_workflows)  # Intersection
+
+        results = list()
+
+        # 2. Find paths within each common workflow
+        for workflow_id in common_workflows:
+            results1 = self.dfs_path_search(component_id_1, component_id_2, workflow_id)
+            results2 = self.dfs_path_search(component_id_2, component_id_1, workflow_id)
+            results.extend(results1)
+            results.extend(results2)
+                
+        for length in results:
+            path_lengths[length] += 1
+            
+
+        # 3. Compute coupling score
+        coupling_score = sum(N_l / (l ** penalty) for l, N_l in path_lengths.items() if l > 0)
+
+        return {
+            "path_lengths": dict(path_lengths),
+            "coupling_score": coupling_score
+        }
+        
+    def get_all_component_ids(self):
+        """Fetches all component_ids of CalculationComponent2 nodes."""
+        query = """
+        MATCH (c:CalculationComponent2)
+        RETURN c.component_id AS component_id
+        """
+        with self.driver.session() as session:
+            result = session.run(query)
+            return [record["component_id"] for record in result]
+        
+    def have_same_prefix(self, s1, s2):
+        parts1 = s1.split("\\")
+        parts2 = s2.split("\\")
+
+        return parts1[:2] == parts2[:2] if len(parts1) >= 2 and len(parts2) >= 2 else False
+
+    
+    def complete_path_analysis(self):
+        component_ids = self.get_all_component_ids()
+        matrix = pd.DataFrame(-1.0, index=component_ids, columns=component_ids)
+        for component_id_1 in component_ids:
+            print(f"Analyzing {component_id_1}")
+            for component_id_2 in component_ids:
+                if component_id_1 == component_id_2:
+                    continue
+                if not self.have_same_prefix(component_id_1, component_id_2):
+                    continue
+                analysis = self.analyze_paths(component_id_1, component_id_2)
+                matrix.at[component_id_1, component_id_2] = analysis["coupling_score"]
+        matrix.to_csv("analysis.csv")
+
+        return matrix
+
+
 
     def save_to_csv(self, data, filename="calculation_component_fan_data.csv"):
         """Saves CalculationComponent fan-in, fan-out, and fan-out set data to a CSV file."""
